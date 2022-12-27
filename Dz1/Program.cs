fa509487dd1d3edb512923b1b5e0ad0e90f845e6
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-27 13:12:44 +0300

    dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿double [,] number = new double [3,4];
+Random rnd = new Random();
+
+{
+for (int i = 0; i < number.GetLength(0); i++)
+{
+    for (int j = 0; j < number.GetLength(1); j++)
+    {
+        number [ i, j ] = rnd.NextDouble() * 10;
+        Console.Write("{0,6:F2}", number[i, j]);
+        Console.WriteLine();
+    }
+
+}
+    
+}
+
+
